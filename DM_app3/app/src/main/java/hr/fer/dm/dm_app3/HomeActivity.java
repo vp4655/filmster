7c00d359bc86e6be5594bc654f9401d04efcf2a6
Manuela <tinker_bell_@outlook.com>
2015-12-17 23:28:45 +0100

    nesto

--- DM_app3/app/src/main/java/hr/fer/dm/dm_app3/HomeActivity.java
+++ DM_app3/app/src/main/java/hr/fer/dm/dm_app3/HomeActivity.java
@@ -1,6 +1,9 @@
 package hr.fer.dm.dm_app3;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageInstaller;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,10 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 //import com.android.volley.Response;
 //import com.android.volley.VolleyError;
@@ -22,6 +32,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,10 +63,15 @@ public class HomeActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private Toolbar toolbar;
 
+    private FrameLayout frameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        frameLayout = (FrameLayout) findViewById( R.id.mainmenu);
+        frameLayout.getForeground().setAlpha( 0);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -124,13 +144,70 @@ public class HomeActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            initiatePopupWindow();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private PopupWindow pw;
+    TextView popUpText;
+    private FrameLayout frameLayoutPopUp;
 
+    private void initiatePopupWindow(){
+        try {
+            //frameLayout = (FrameLayout) findViewById( R.id.mainmenu_popup);
+            //frameLayout.getForeground().setAlpha( 200);
+            //We need to get the instance of the LayoutInflater, use the context of this activity
+            LayoutInflater inflater = (LayoutInflater) HomeActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //Inflate the view from a predefined XML layout
+            View layout = inflater.inflate(R.layout.pop_up, (ViewGroup) findViewById(R.id.popup_element));
+            // create a 300px width and 470px height PopupWindow
+            DisplayMetrics displaymetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+            int height = displaymetrics.heightPixels;
+            int width = displaymetrics.widthPixels;
+            pw = new PopupWindow(layout, Math.round(width*0.7f), Math.round(height*0.7f), true);
+            // display the popup in the center
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+            //popUpText = (TextView) layout.findViewById(R.id.tvPopUpTxt);
+           // popUpText.setText(readFile("about.txt"));
+
+            Button cancelButton = (Button) layout.findViewById(R.id.end_data_send_button);
+            //makeBlack(cancelButton);
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    frameLayout.getForeground().setAlpha(0);
+                    pw.dismiss();
+                }
+            });
+
+            frameLayout.getForeground().setAlpha( 210); // dim
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
     //*************************************************//
 
     /**
