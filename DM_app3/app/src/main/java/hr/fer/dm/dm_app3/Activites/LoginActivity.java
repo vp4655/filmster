2e41db3656c4863b810316635ff6514f41e3d32d
Manuela <tinker_bell_@outlook.com>
2016-01-01 23:07:50 +0100

    custom login button

--- DM_app3/app/src/main/java/hr/fer/dm/dm_app3/Activites/LoginActivity.java
+++ DM_app3/app/src/main/java/hr/fer/dm/dm_app3/Activites/LoginActivity.java
@@ -1,5 +1,6 @@
 package hr.fer.dm.dm_app3.Activites;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -32,6 +34,9 @@ public class LoginActivity extends AppCompatActivity {
     private TextView info;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+
+    private Button myloginButton;
+    private ProgressDialog pDialog;
 
     // odmah na početku definiramo bindanje
     @Bind(R.id.btnSkip) Button skipButton;
@@ -54,7 +59,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onInitialized() {
                 AccessToken accessToken = AccessToken.getCurrentAccessToken();
-                if(accessToken == null){
+                if (accessToken == null) {
                     //System.out.println("not logged in yet");
                 } else {
                     //System.out.println("Logged in");
@@ -68,37 +73,85 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         setContentView(R.layout.activity_login);
-        info = (TextView)findViewById(R.id.info);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+
+        // za onaj plavi login button
+
+//        info = (TextView)findViewById(R.id.info);
+//        loginButton = (LoginButton)findViewById(R.id.login_button);
+//
+//        callbackManager = CallbackManager.Factory.create();
+
+//        List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+//        loginButton.setReadPermissions(permissionNeeds);
+//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+////                info.setText(
+////                        "\n\n\nUser ID: "
+////                                + loginResult.getAccessToken().getUserId()
+////                                + "\n" +
+////                                "Auth Token: "
+////                                + loginResult.getAccessToken().getToken()
+////                );
+//                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+//                finish();
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//                info.setText("Login attempt canceled.");
+//            }
+//
+//            @Override
+//            public void onError(FacebookException exception) {
+//                info.setText("Login attempt failed.");
+//            }
+//        });
+        //***********************************************************//
+
+        // handmade button
+
+        myloginButton = (Button)findViewById(R.id.login_button_my);
+        myloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Login with facebook on clicking custom button
+                // izgleda da sam facebook sdk ima svoj loader
+//                pDialog = new ProgressDialog(LoginActivity.this);
+//                // Showing progress dialog before making http request
+//                pDialog.setMessage("Logging in...");
+//                pDialog.show();
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile", "user_friends"));
+                //hidePDialog();
+            }
+        });
 
         callbackManager = CallbackManager.Factory.create();
-        List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
-        loginButton.setReadPermissions(permissionNeeds);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-//                info.setText(
-//                        "\n\n\nUser ID: "
-//                                + loginResult.getAccessToken().getUserId()
-//                                + "\n" +
-//                                "Auth Token: "
-//                                + loginResult.getAccessToken().getToken()
-//                );
+
                 startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                 finish();
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt canceled.");
+                // do sth
             }
 
             @Override
             public void onError(FacebookException exception) {
-                info.setText("Login attempt failed.");
+                // do sth
             }
         });
+
+
         //***********************************************************//
+
+
         ButterKnife.bind(this);                         // u onCreate dodati ovo kako bi se bindanje obavilo
 
         skipButton.setOnClickListener(
@@ -149,6 +202,14 @@ public class LoginActivity extends AppCompatActivity {
         moveTaskToBack(true);
         //moveTaskToBack(true) leaves your back stack as it is, just puts all Activities in background (same as if user pressed Home button).
     }
+
+    protected void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
+    }
+
 //
 //    @Override
 //    protected void onPause() {
