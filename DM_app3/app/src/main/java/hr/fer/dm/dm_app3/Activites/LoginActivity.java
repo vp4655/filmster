04cacea70f8d5f4f7f708452c159e4fe6b70879f
Manuela <tinker_bell_@outlook.com>
2016-01-01 21:42:21 +0100

    facebook login

--- DM_app3/app/src/main/java/hr/fer/dm/dm_app3/Activites/LoginActivity.java
+++ DM_app3/app/src/main/java/hr/fer/dm/dm_app3/Activites/LoginActivity.java
@@ -3,8 +3,24 @@ package hr.fer.dm.dm_app3.Activites;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,16 +28,57 @@ import hr.fer.dm.dm_app3.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
     // odmah na početku definiramo bindanje
     @Bind(R.id.btnSkip) Button skipButton;
     @Bind(R.id.actor_btn) Button actorButton;
+
+//    @Bind(R.id.login_button) LoginButton loginButton;
+//    @Bind(R.id.info) Button info;
 
     public static final String IMAGE_URL = "http://image.tmdb.org/t/p/w500";
     public static final String ACTOR_DETAIL_KEY = "actor";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //***********************FACEBOOK****************************//
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
         setContentView(R.layout.activity_login);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        callbackManager = CallbackManager.Factory.create();
+        List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+        loginButton.setReadPermissions(permissionNeeds);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "\n\n\nUser ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                info.setText("Login attempt failed.");
+            }
+        });
+        //***********************************************************//
         ButterKnife.bind(this);                         // u onCreate dodati ovo kako bi se bindanje obavilo
 
         skipButton.setOnClickListener(
@@ -47,4 +104,27 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        // Logs 'install' and 'app activate' App Events.
+//        AppEventsLogger.activateApp(this);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        // Logs 'app deactivate' App Event.
+//        AppEventsLogger.deactivateApp(this);
+//    }
 }
